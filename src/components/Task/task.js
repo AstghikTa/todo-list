a37ee9dd8c5067a5d53fc45e13f8e999f6c14d82
Astghik Tadevosyan <astx496@gmail.com>
2024-05-06 15:07:50 +0400

    Errors have been corrected

--- src/components/Task/task.js
+++ src/components/Task/task.js
@@ -10,7 +10,7 @@ export default {
         return this.data.created_at.slice(0, 10)
       },
       dataDate(){
-      return  this.data.date ?  this.data.date.slice(0, 10) : ''
+      return  this.data.date ?  this.data.date.slice(0, 10) : '' || 'none'
       }
     },
     methods: {
